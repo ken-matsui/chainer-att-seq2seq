92b711ccca35acdc0fb2eaeb62c5831e908e927e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-11-07 00:37:20 +0900

    modify seq2seq

--- seq2seq.py
+++ seq2seq.py
@@ -1,5 +1,10 @@
 # coding:utf-8
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import math
 import os
 import random
@@ -9,6 +14,8 @@ import time
 import numpy as np
 import tensorflow as tf
 import tensorflow.python.platform
+from tensorflow.contrib import rnn
+from tensorflow.contrib import legacy_seq2seq
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -18,13 +25,21 @@ from tqdm import tqdm
 import parse
 import mecab
 import split
-import seq2seq_model
 
-tf.app.flags.DEFINE_boolean("parse", False, "Run a parse if this is set True")
+# tf.app.flags.DEFINE_string('変数名', 'デフォルト値', """説明文""")
+# python script.py --'変数名' ok => tf.app.flags.FLAGS.'変数名' == 'ok'
+tf.app.flags.DEFINE_boolean("parse", False, "Run a parse if this is set to True")
+tf.app.flags.DEFINE_boolean("decode", False, "Set to True for interactive decoding.")
 FLAGS = tf.app.flags.FLAGS
 
+
 def seq2seq():
-	print("hoge")
+	if FLAGS.decode:
+		print("decode()")
+		# decode()
+	else:
+		print("train()")
+		# train()
 
 def main(_):
 	if FLAGS.parse:
@@ -34,9 +49,14 @@ def main(_):
 		mecab.main()
 		print("split...")
 		split.main()
-
-	print("seq2seq...")
-	seq2seq()
+		print("seq2seq...")
+		seq2seq()
+	else:
+		print("skip parse")
+		print("skip mecab")
+		print("skip split")
+		print("seq2seq...")
+		seq2seq()
 
 if __name__ == '__main__':
 	tf.app.run()
