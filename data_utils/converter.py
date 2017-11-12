7b5189eab249735b81b94638c929c8f59418b2c6
matken11235 <matken11235@gmail.com>
2017-11-12 22:08:42 +0900

    Make data_utils dir, Move .py to it dir

--- data_utils/converter.py
+++ data_utils/converter.py
@@ -1,64 +1,57 @@
 import numpy as xp # TODO: cupyにも対応する
+import MeCab
 
 # データ変換クラスの定義
 class DataConverter:
-	def __init__(self, batch_col_size=15):
-		'''
-		クラスの初期化
-		:param batch_col_size: 学習時のミニバッチ単語数サイズ
-		'''
+	def __init__(self, batch_col_size):
+		# クラスの初期化
+		# :param batch_col_size: 学習時のミニバッチ単語数サイズ
 		self.mecab = MeCab.Tagger('-d /usr/local/Cellar/mecab-ipadic/2.7.0-20070801/lib/mecab/dic/ipadic') # 形態素解析器
-		self.vocab = { "<eos>": 0, "<unk>": 1 } # 単語辞書
+		self.vocab = {"<eos>": 0, "<unk>": 1} # 単語辞書
 		self.batch_col_size = batch_col_size
 
-	def load(self, inputs, outputs):
-		'''
-		学習時に、教師データを読み込んでミニバッチサイズに対応したNumpy配列に変換する
-		:param inputs: inputデータ(list)
-		:param outputs: outputデータ(list)
-		'''
+	def load(self, data):
+		# 学習時に、教師データを読み込んでミニバッチサイズに対応したNumpy配列に変換する
+		# :param data: 対話データ
 		# 単語辞書の登録
-		self.vocab = { "<eos>": 0, "<unk>": 1 } # 単語辞書を初期化
+		self.vocab = {"<eos>": 0, "<unk>": 1} # 単語辞書を初期化
+		for d in data:
+			sentences = [d[0], d[1]] # 入力文、返答文
+			for sentence in sentences:
+				sentence_words = self.sentence2words(sentence) # 文章を単語に分解する
+				for word in sentence_words:
+					if word not in self.vocab:
+						self.vocab[word] = len(self.vocab)
+		# 教師データのID化と整理
 		queries, responses = [], []
-		for sentence_in, sentence_out in zip(inputs, outputs):
-			words_in = self.sentence2words(sentence_in)
-			words_out = self.sentence2words(sentence_out)
-			# 教師データのID化と整理
-			queries.append(self.sentence2ids(words=words_in, sentence_type="query"))
-			responses.append(self.sentence2ids(words=words_out, sentence_type="response"))
-			for word_in, word_out in zip(words_in, words_out):
-				if word_in not in self.vocab:
-					self.vocab[word_in] = len(self.vocab)
-				if word_out not in self.vocab:
-					self.vocab[word_out] = len(self.vocab)
+		for d in data:
+			query, response = d[0], d[1] #  エンコード文、デコード文
+			queries.append(self.sentence2ids(sentence=query, train=True, sentence_type="query"))
+			responses.append(self.sentence2ids(sentence=response, train=True, sentence_type="response"))
 		self.train_queries = xp.vstack(queries)
 		self.train_responses = xp.vstack(responses)
 
 	def sentence2words(self, sentence):
-		'''
-		文章を単語の配列にして返却する
-		:param sentence: 文章文字列
-		:return: mecabでparseして単語ごとに分割したsentence
-		'''
+		# 文章を単語の配列にして返却する
+		# :param sentence: 文章文字列
 		sentence_words = []
 		for m in self.mecab.parse(sentence).split("\n"): # 形態素解析で単語に分解する
 			w = m.split("\t")[0].lower() # 単語
-			if len(w) == 0 or w == "eos": # 不正文字、eosは省略
+			if len(w) == 0 or w == "eos": # 不正文字、EOSは省略
 				continue
 			sentence_words.append(w)
 		sentence_words.append("<eos>") # 最後にvocabに登録している<eos>を代入する
 		return sentence_words
 
-	def sentence2ids(self, words, train=True, sentence_type="query"):
-		'''
-		文章を単語IDのNumpy配列に変換して返却する
-		:param sentence: 文章文字列
-		:param train: 学習用かどうか
-		:sentence_type: 学習用でミニバッチ対応のためのサイズ補填方向をクエリー・レスポンスで変更するため"query"or"response"を指定　
-		:return: 単語IDのNumpy配列
-		'''
+	def sentence2ids(self, sentence, train=True, sentence_type="query"):
+		# 文章を単語IDのNumpy配列に変換して返却する
+		# :param sentence: 文章文字列
+		# :param train: 学習用かどうか
+		# :sentence_type: 学習用でミニバッチ対応のためのサイズ補填方向をクエリー・レスポンスで変更するため"query"or"response"を指定　
+		# :return: 単語IDのNumpy配列
 		ids = [] # 単語IDに変換して格納する配列
-		for word in words:
+		sentence_words = self.sentence2words(sentence) # 文章を単語に分解する
+		for word in sentence_words:
 			if word in self.vocab: # 単語辞書に存在する単語ならば、IDに変換する
 				ids.append(self.vocab[word])
 			else: # 単語辞書に存在しない単語ならば、<unk>に変換する
@@ -77,13 +70,10 @@ class DataConverter:
 			ids = xp.array([ids], dtype="int32")
 		return ids
 
-	# 予測時に使用する関数
 	def ids2words(self, ids):
-		'''
-		予測時に、単語IDのNumpy配列を単語に変換して返却する
-		:param ids: 単語IDのNumpy配列
-		:return: 単語の配列
-		'''
+		# 予測時に、単語IDのNumpy配列を単語に変換して返却する
+		# :param ids: 単語IDのNumpy配列
+		# :return: 単語の配列
 		words = [] # 単語を格納する配列
 		for i in ids: # 順番に単語IDを単語辞書から参照して単語に変換する
 			words.append(list(self.vocab.keys())[list(self.vocab.values()).index(i)])
