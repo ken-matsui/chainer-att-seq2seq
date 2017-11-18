76199ae2330fd8cda0c006dcd201350a69c17898
matken11235 <matken11235@gmail.com>
2017-11-19 05:20:13 +0900

    parserでのIDファイルも生成するようにした

--- data_utils/converter.py
+++ data_utils/converter.py
@@ -3,12 +3,13 @@
 import os
 
 from chainer import cuda
+# import cupy as cp
 import numpy as np
 import MeCab
 
 FLAG_GPU = False
 if FLAG_GPU:
-	xp = cuda.cupy
+	xp = cp
 	cuda.get_device(0).use()
 else:
 	xp = np
@@ -30,27 +31,32 @@ class DataConverter:
 		学習時に、教師データを読み込んでミニバッチサイズに対応したNumpy配列に変換する
 		:param path: dataがあるディレクトリ
 		'''
-		# 対話データを取り出す
-		with open(path + 'query.txt', 'r') as fq, open(path + 'response.txt', 'r') as fr:
-			que, res = fq.readlines(), fr.readlines()
-		data = list(zip(que, res))
-		self.teacher_num = len(data)
+		# 対話データ(ID版)を取り出す
+		with open(path + 'query_id.txt', 'r') as fqid, open(path + 'response_id.txt', 'r') as frid:
+			queid, resid = fqid.readlines(), frid.readlines()
+		self.teacher_num = len(list(zip(queid, resid)))
+		queries, responses = [], []
+		for q, r in zip(queid, resid):
+			# ミニバッチ対応のため，単語数サイズを調整してNumpy変換する
+			query = list(map(int, q.replace('\n', '').split(',')))
+			queries.append(self.batch_ids(query))
+			respo = list(map(int, q.replace('\n', '').split(',')))
+			responses.append(self.batch_ids(respo))
+		self.train_queries = xp.vstack(queries)
+		self.train_responses = xp.vstack(responses)
 
 		# 単語辞書データを取り出す
 		with open(path + 'vocab.txt', 'r') as f:
 			lines = f.readlines()
 		for line in lines:
-			if line: # 空行を弾く
-				self.vocab.append(line.replace("\n", ""))
+			self.vocab.append(line.replace("\n", ""))
 
-		# 教師データのID化と整理
-		queries, responses = [], []
-		for d in data:
-			query, response = d[0], d[1] #  エンコード文、デコード文
-			queries.append(self.sentence2ids(sentence=query, train=True, sentence_type="query"))
-			responses.append(self.sentence2ids(sentence=response, train=True, sentence_type="response"))
-		self.train_queries = xp.vstack(queries)
-		self.train_responses = xp.vstack(responses)
+	def batch_ids(self, ids):
+		if len(ids) > self.batch_col_size: # ミニバッチ単語サイズになるように先頭から削る
+			del ids[0:len(ids) - self.batch_col_size]
+		else: # ミニバッチ単語サイズになるように先頭に付け足す
+			ids = ([-1] * (self.batch_col_size - len(ids))) + ids
+		return ids
 
 	def sentence2words(self, sentence):
 		'''
@@ -61,18 +67,16 @@ class DataConverter:
 		sentence_words = []
 		for m in self.mecab.parse(sentence).split("\n"): # 形態素解析で単語に分解する
 			w = m.split("\t")[0].lower() # 単語
-			if (len(w) == 0) or (w is "eos"): # 不正文字、EOSは省略
+			if (len(w) == 0) or (w == "eos"): # 不正文字、EOSは省略
 				continue
 			sentence_words.append(w)
 		sentence_words.append("<eos>") # 最後にvocabに登録している<eos>を代入する
 		return sentence_words
 
-	def sentence2ids(self, sentence, train=True, sentence_type="query"):
+	def sentence2ids(self, sentence):
 		'''
 		文章を単語IDのNumpy配列に変換して返却する
 		:param sentence: 文章文字列
-		:param train: 学習用かどうか
-		:sentence_type: 学習用でミニバッチ対応のためのサイズ補填方向をクエリー・レスポンスで変更するため"query"or"response"を指定　
 		:return: 単語IDのNumpy配列
 		'''
 		ids = [] # 単語IDに変換して格納する配列
@@ -82,18 +86,7 @@ class DataConverter:
 				ids.append(self.vocab.index(word))
 			else: # 単語辞書に存在しない単語ならば、<unk>のIDに変換する
 				ids.append(self.vocab.index("<unk>"))
-		# 学習時は、ミニバッチ対応のため、単語数サイズを調整してNumpy変換する
-		if train:
-			if sentence_type == "query": # クエリーの場合は前方にミニバッチ単語数サイズになるまで-1を補填する
-				while len(ids) > self.batch_col_size: # ミニバッチ単語サイズよりも大きければ、ミニバッチ単語サイズになるまで先頭から削る
-					ids.pop(0)
-				ids = xp.array([-1]*(self.batch_col_size-len(ids))+ids, dtype="int32")
-			elif sentence_type == "response": # レスポンスの場合は後方にミニバッチ単語数サイズになるまで-1を補填する
-				while len(ids) > self.batch_col_size: # ミニバッチ単語サイズよりも大きければ、ミニバッチ単語サイズになるまで末尾から削る
-					ids.pop()
-				ids = xp.array(ids+[-1]*(self.batch_col_size-len(ids)), dtype="int32")
-		else: # 予測時は、そのままNumpy変換する
-			ids = xp.array([ids], dtype="int32")
+		ids = xp.array([ids], dtype="int32")
 		return ids
 
 	def ids2words(self, ids):
