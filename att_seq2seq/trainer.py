ed2842e2177ce856f16fb73c57119d9481debdec
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-11-21 01:43:58 +0900

    model.pyの__call__を解体, converter.pyの一部メソッドをdecoder.pyへ移行

--- att_seq2seq/trainer.py
+++ att_seq2seq/trainer.py
@@ -5,10 +5,20 @@ import re
 import datetime
 from os.path import join, dirname
 
-from chainer import optimizer, optimizers, serializers
+import chainer.functions as F
+from chainer import optimizer, optimizers, serializers, Variable
 import numpy as np
 from dotenv import load_dotenv
 import slackweb
+
+FLAG_GPU = False
+if FLAG_GPU:
+	import cupy as cp
+	xp = cp
+	cuda.get_device(0).use()
+else:
+	import numpy as np
+	xp = np
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -37,15 +47,36 @@ class Trainer(object):
 			# ミニバッチ学習
 			perm = np.random.permutation(teacher_num) # ランダムな整数列リストを取得
 			total_loss = 0
+			total_accuracy = 0
 			for i in range(0, teacher_num, batch_size):
-				enc_words = queries[perm[i:i+batch_size]]
-				dec_words = responses[perm[i:i+batch_size]]
+				# モデルの勾配などをリセット
 				self.model.reset()
-				loss = self.model(enc_words=enc_words, dec_words=dec_words, train=True)
+				# 整数列リストからそれぞれのwordを取得
+				enc_words = queries[perm[i:i+batch_size]].T
+				dec_words = responses[perm[i:i+batch_size]].T
+				# エンコード時のバッチサイズ
+				encode_batch_size = len(enc_words[0])
+				# 発話リスト内の単語をVariable型に変更
+				enc_words = [Variable(xp.array(row, dtype='int32')) for row in enc_words]
+				# エンコードの計算
+				self.model.encode(enc_words, encode_batch_size)
+				# <eos>をデコーダーに読み込ませる
+				t = Variable(xp.array([0] * encode_batch_size, dtype='int32'))
+				# 損失の初期化
+				loss = Variable(xp.zeros((), dtype='float32'))
+				# 精度の初期化
+				accuracy = Variable(xp.zeros((), dtype='float32'))
+				# １単語ずつデコードする
+				for w in dec_words:
+					y = self.model.decode(t)
+					t = Variable(xp.array(w, dtype='int32')) # 正解単語をVariable型に変換
+					loss += F.softmax_cross_entropy(y, t) # 正解単語と予測単語を照らし合わせて損失を計算
+					accuracy += F.accuracy(y, t) # 精度の計算
 				loss.backward()
 				loss.unchain_backward()
-				total_loss += loss.data
 				opt.update()
+				total_loss += loss.data
+				total_accuracy += accuracy.data
 			if (epoch+1) % 10 == 0:
 				# モデルの保存
 				if flag_gpu: # modelをCPUでも使えるように
@@ -54,7 +85,7 @@ class Trainer(object):
 				if flag_gpu:
 					self.model.to_gpu(0)
 			ed = datetime.datetime.now()
-			data = "epoch: {}\tloss: {}\ttime: {}".format(epoch+1, total_loss, ed-st)
+			data = "epoch: {}\tloss: {}\taccuracy: {}\ttime: {}".format(epoch+1, int(total_loss), int(accuracy), ed-st)
 			slack.notify(text=data)
 			print(data)
 			st = datetime.datetime.now()
