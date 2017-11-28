80ecf491fd399277443c0e7fb319132bdf310253
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-11-28 21:03:45 +0900

    fix bug

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ group.add_argument('-d', '--decode', default=False, action='store_true',
 				   help='Decode mode if this flag is set')
 parser.add_argument('-p', '--parse', default=False, action='store_true',
 					help='Parse mode if this flag is set')
-parser.add_argument('-r', '--resume', type=int, default=-1,
-					help="Select npz file's number. You can specify the latest model file by specifying 0.")
+parser.add_argument('-r', '--resume', default=False, action='store_true',
+					help="Resume mode if this flag is set")
 parser.add_argument('-e', '--epoch', type=int, default=500,
 					help="Number of epoch")
 parser.add_argument('-es', '--embed_size', type=int, default=500,
@@ -37,6 +37,8 @@ parser.add_argument('-ri', '--resid_file', default='./data/response_id.txt',
 					help="Directory to response file")
 parser.add_argument('-o', '--out', default='./result/',
 					help="Directory to output the result")
+parser.add_argument('-s', '--select', type=int, default=0,
+					help="Select npz file.")
 parser.add_argument('--tag', default='',
 					help="TAG")
 parser.add_argument('-g', '--gpu', default=False, action='store_true',
@@ -64,8 +66,8 @@ def main():
 		print('')
 		# 学習用データを読み込む
 		queries, responses = load_queres()
-		if FLAGS.resume != -1:
-			if FLAGS.resume == 0:
+		if FLAGS.resume:
+			if FLAGS.select == 0:
 				# 最新のモデルデータを使用する．
 				files = [splitext(relpath(s, FLAGS.out))[0] for s in glob(FLAGS.out + "*.npz")]
 				num = max(list(map(int, files)))
